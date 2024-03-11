cefe8d8c662c6ab4dafebf5ad5c4da4c07588477
mwizerwa77 <mwizerwa77@gmail.com>
2024-03-11 10:15:41 +0200

    it is initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports.add = function add(num1,num2,num3,num4){
+    return num1+num2+num3+num4
+}
+module.exports.sub =function sub(num1,num2){
+    return num1-num2
+}
+
+module.exports.multiply = function multiply(num1,num2){
+    return num1*num2
+}
+
